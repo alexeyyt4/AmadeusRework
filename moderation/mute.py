c27618d28b19e411f87eb2e8a19a6e68279bafc5
alexeyyt4 <122479408+alexeyyt4@users.noreply.github.com>
2023-09-03 17:31:06 +0300

    Фикс багов в муте. Работает.

--- moderation/mute.py
+++ moderation/mute.py
@@ -1,19 +1,22 @@
 import disnake
 from disnake.ext import commands
 import aiosqlite
-import datatime
+import datetime
 
-class WarningCog(commands.Cog):
+class MuteCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command()
     async def mute(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member, time: str, reason: str):
-        time = datatime.datatime.now() + datatime.datatime.delta(minutes=int(time))
+        time = datetime.datetime.now() + datetime.timedelta(minutes=int(time))
         await member.timeout(reason=reason, until=time)
         embed = disnake.Embed(
-            title=f"Был замучен {member.mention}",
+            title=f"Был замучен {member.global_name}",
             description=f"{inter.author.mention} замутил данного человека до {time}, за {reason}",
-            timestamp=datatime.datatime.now(),
+            timestamp=datetime.datetime.now(),
         )
         await inter.response.send_message(embed=embed)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(MuteCog(bot))
