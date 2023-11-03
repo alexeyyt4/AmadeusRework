fb578ae6ecf054c05fd3a9e30bdeb717784f54c0
volodgt8.scp@gmail.com <volodgt8.scp@gmail.com>
2023-11-03 12:57:27 +0200

    update help.py

--- info/help.py
+++ info/help.py
@@ -19,38 +19,39 @@ class Select(disnake.ui.Select):
             min_values=1,
             options=options,
         )
-    
-    async def callback(self, inter: MessageInteraction):
-        await inter.response.defer()
-        if Select.values == "1":
-            embed = disnake.Embed(
-                title="Модерация🛡️",
-                description="**/ban - Забанить пользователя'\n/kick - Кикнуть пользователя\n/clear - Очистка сообщений (двухнедельные не удаляются. хотя могло и измениться за время написания этого текста)\n/warn и /unwarn - Заварнить/Снять варн пользователю ( для снятия варна нужно знать ID варна )\n/warnings - Показывает предупреждения пользователя",
-                timestamp=f"AmBot v1.0 (02.11.2023)"
-            )
-            await inter.channel.send_message(embed=embed)
-        if Select.values == "2":
-            embed = disnake.Embed(
-                title="Фан🎪",
-                description="/ping - проверить пинг бота\n/roll - Всем знакомый ролл ( можно задать максималное число )",
-                timestamp=f"AmBot v1.0 (02.11.2023)"
-            )
-            await inter.channel.send_message(embed=embed)
-        if Select.values == "3":
-            embed = disnake.Embed(
-                title="Информация🎩",
-                description="/serverinfo - Дает информацию о сервере\n/userinfo - Дает информацию о участнике ( Надо чтобы указанный вами человек находился на сервере. )",
-                timestamp=f"AmBot v1.0 (02.11.2023)"
-            )
-            await inter.channel.send_message(embed=embed)
-        if Select.values == "4":
-            await inter.channel.send_message("https://discord.gg/8PafGdMnY8")
-
 class HelpCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_select_option(self, interaction):
+         if interaction.custom_id == "help":
+          if interaction.values[0] == "1":
+              embed = disnake.Embed(
+                  title="Модерация🛡️",
+                  description="**/ban - Забанить пользователя'\n/kick - Кикнуть пользователя\n/clear - Очистка сообщений (двухнедельные не удаляются. хотя могло и измениться за время написания этого текста)\n/warn и /unwarn - Заварнить/Снять варн пользователю ( для снятия варна нужно знать ID варна )\n/warnings - Показывает предупреждения пользователя",
+                  timestamp=f"AmBot v1.0 (02.11.2023)"
+              )
+              await inter.response.send_message(embed=embed)
+          if interaction.values[0] == "2":
+              embed = disnake.Embed(
+                  title="Фан🎪",
+                  description="/ping - проверить пинг бота\n/roll - Всем знакомый ролл ( можно задать максималное число )",
+                  timestamp=f"AmBot v1.0 (02.11.2023)"
+              )
+              await interaction.response.send_message(embed=embed)
+          if interaction.values[0] == "3":
+              embed = disnake.Embed(
+                  title="Информация🎩",
+                  description="/serverinfo - Дает информацию о сервере\n/userinfo - Дает информацию о участнике ( Надо чтобы указанный вами человек находился на сервере. )",
+                  timestamp=f"AmBot v1.0 (02.11.2023)"
+            )
+              await inter.channel.send_message(embed=embed)
+          if Select.values == "4":
+              await interaction.response.send_message("https://discord.gg/8PafGdMnY8")
 
     @commands.slash_command(name="help", description="Учебник по командам")
+    
     async def help(self, ctx):
         view = disnake.ui.View(timeout=None)
         view.add_item(Select())
